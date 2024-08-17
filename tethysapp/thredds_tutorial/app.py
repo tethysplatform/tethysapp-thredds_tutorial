59b0ec76d750dc2af7df22cb84b3a851f7a4b2bc
jakeymac <jakeymacaroni@gmail.com>
2024-08-17 16:21:18 -0600

    updates for tethys 4.3

--- tethysapp/thredds_tutorial/app.py
+++ tethysapp/thredds_tutorial/app.py
@@ -1,34 +1,18 @@
-from tethys_sdk.base import TethysAppBase, url_map_maker
+from tethys_sdk.base import TethysAppBase
 
 
-class ThreddsTutorial(TethysAppBase):
+class App(TethysAppBase):
     """
     Tethys app class for Thredds Tutorial.
     """
 
     name = 'Thredds Tutorial'
-    index = 'thredds_tutorial:home'
-    icon = 'thredds_tutorial/images/icon.gif'
-    package = 'thredds_tutorial'
-    root_url = 'thredds-tutorial'
-    color = '#8e44ad'
     description = ''
+    package = 'thredds_tutorial'  # WARNING: Do not change this value
+    index = 'home'
+    icon = f'{package}/images/icon.gif'
+    root_url = 'thredds-tutorial'
+    color = '#c23616'
     tags = ''
     enable_feedback = False
     feedback_emails = []
-
-    def url_maps(self):
-        """
-        Add controllers
-        """
-        UrlMap = url_map_maker(self.root_url)
-
-        url_maps = (
-            UrlMap(
-                name='home',
-                url='thredds-tutorial',
-                controller='thredds_tutorial.controllers.home'
-            ),
-        )
-
-        return url_maps
